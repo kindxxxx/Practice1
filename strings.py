8e4585a25eb161e0a4df8937edecb5eae2e7b5f9
kindxxxx <sava.molchanov.07@gmail.com>
2026-04-23 10:55:09 +0500

    some examples of work with strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+print ("Hello World")
+
+a="example string 123lol"
+print (a)
+
+b="awesome"
+print(b[3:])
+
+z="Yerzhas owes me pack of nic! i want to record it in my git "
+print(z.upper())
+
+text="my gf Enka"
+print("I love " + text)
